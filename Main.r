7bc88157905f983b1ae616c3c3884c654085be31
alexgeWU <age@wustl.edu>
2025-12-14 17:25:23 -0600

    data visualization done? box plot histogram and comparison of pairs

--- Main.r
+++ Main.r
@@ -3,20 +3,50 @@ data("iris")
 
 speciesID <- as.numeric(as.factor(iris$Species))
 
+par(mfrow = c(1,1), mar = c(4, 4, 2, 1))
 plot(iris$Sepal.Length, iris$Sepal.Width,
-     pch = speciesID,
+     pch = c(0, 1, 2)[speciesID],
      col = speciesID,
      xlab = "Sepal.Length",
      ylab = "Sepal.Width",
      main = "Sepal Length vs Width")
+legend("topright",
+       legend = levels(iris$Species),
+       pch = c(0, 0, 0),
+       col = speciesID,
+       title = "Species",
+       bg = "white")
 
+par(mfrow = c(1,1), mar = c(0, 0, 0, 0))
 pairs(iris[, 1:4],
       col = speciesID,
       pch = 16,
+      cex = .75,
+      labels = c("Sepal Length", "Sepal Width", "Petal Length", "Petal Width"),
       main = "Iris Visualization")
-      
+
+setosa <- iris[iris$Species == "setosa",]
+versicolor <- iris[iris$Species == "versicolor",]
+virginica <- iris[iris$Species == "virginica",]
+
+par(mfrow = c(2, 2), mar = c(2, 4, 2, 1))
 boxplot(Sepal.Length ~ Species,
         data = iris,
-        main = "Sepal Length Data Box Plots",
+        main = "Sepal Length by Species Box Plots",
         ylab = "Sepal Length cm",
+        xlab = "Species")
+boxplot(Sepal.Width ~ Species,
+        data = iris,
+        main = "Sepal Width by Species Box Plots",
+        ylab = "Sepal Width cm",
+        xlab = "Species")
+boxplot(Petal.Length ~ Species,
+        data = iris,
+        main = "Petal Length by Species Box Plots",
+        ylab = "Petal Length cm",
+        xlab = "Species")
+boxplot(Petal.Width ~ Species,
+        data = iris,
+        main = "Petal Width by Species Box Plots",
+        ylab = "Petal Width cm",
         xlab = "Species")
